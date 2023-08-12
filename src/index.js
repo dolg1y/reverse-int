0c21fc63642b7f9d8feba5710e6496aa61bcc85b
Vadim <114777740+dolg1y@users.noreply.github.com>
2023-08-12 13:09:36 +0000

    init: create task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    n = n + "";
+    if(n < 0) {
+        return n.split("").reverse().join("").replace('-','');
+    }
+    else {
+        return n.split("").reverse().join("");
+    }
 }
